2687aaf9932ebffe69b8c49aa49fa575acd1b39f
David Junior <david.lourenco94@gmail.com>
2022-06-20 23:59:41 -0300

    learning props React

---
+++ Front-End/bloco-10-introducao-a-react/dia-2-componentes-react/fixation-exercises-10.2/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Image from './Image';
+
+class App extends React.Component {
+  render() {
+    return(
+      <div>
+      <Image source="cat.jpg" alternativeText="Cute cat staring" />
+      </div>
+    );
+  }
+}
+
+export default App;
